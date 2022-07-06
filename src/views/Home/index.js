c7b294343a8103900d5b1200dc9251e4b198ab6d
gmanriqe <gmanriqe@gmail.com>
2022-07-06 11:39:47 -0500

    API - consumo de api con fetch y envio por params.  solucion de cors en reactjs y envio de token

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,18 +1,20 @@
-import "flatpickr/dist/themes/material_green.css";
+import 'flatpickr/dist/themes/material_green.css';
+import dayjs from 'dayjs'; // dayjs
 
-import Flatpickr from "react-flatpickr";
+import Flatpickr from 'react-flatpickr'; // flatpickr
 import Select from 'react-select' // react select
 import { useEffect, useState } from 'react';
 
 import { Spanish } from 'flatpickr/dist/l10n/es.js'; // configure language for flatpickr
-import { Banner } from "../../components/Banner";
-import { Paises as paises } from "../../mock/Country";
+import { Banner } from '../../components/Banner';
+import { Paises as paises } from '../../mock/Country';
 
+import { formData } from '../../mock/Token';
 
-import { formData } from "../../mock/Token";
+dayjs.locale('es')
 
 const Home = () => {
-    const [stateDepartureDate, setStateDepartureDate] = useState(new Date());
+    const [departureDate, setDepartureDate] = useState(new Date());
     const [dateEnd, setDateEnd] = useState(new Date());
     const [token, setToken] = useState(null);
 
@@ -57,25 +59,30 @@ const Home = () => {
         )
     }
 
-    const handlerSubmitMainSearch = (evt) => {
+    async function handlerSubmitMainSearch(evt) {
         evt.preventDefault();
+        
+        const { originLocationCode, destinationLocationCode } = evt.currentTarget
+        const dateVal = dayjs(new Date(departureDate)).format('YYYY-MM-DD')
 
-        const $form = document.querySelector('#form-main-search');
-        const formData = new FormData($form)
-        formData.append('adults','1');
-
-        for (var pair of formData.entries()) {
-          console.log(pair[0]+ ', ' + pair[1]);
-        }
+        const response = await fetch(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originLocationCode.value}&destinationLocationCode=${destinationLocationCode.value}&departureDate=${dateVal}&adults=1`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        console.log(token)
+        const data = await response.json();
+        console.log(data)
     }
 
     return (
         <>
-            <Banner title="VUELOS AEREOS" />
+            <Banner title='VUELOS AEREOS' />
             <section className='p-lg'>
                 <div className='container mx-auto'>
                     <div className='card p-sm'>
-                        <form id="form-main-search" className='grid grid-cols-1 lg:grid-cols-2 gap-4' onSubmit={handlerSubmitMainSearch} >
+                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-4' onSubmit={handlerSubmitMainSearch} >
                             <div className='form-group'>
                                 <Select className='form-control-select' options={optionsDeparture} name='originLocationCode' />
                                 <span className='message-error'>Campo obligatorio</span>
@@ -88,13 +95,13 @@ const Home = () => {
                                 <Flatpickr
                                     className='form-control'
                                     name='departureDate'
-                                    value={stateDepartureDate}
+                                    value={departureDate}
                                     options={{
                                         enableTime: false,
-                                        dateFormat: "l, d M",
+                                        dateFormat: 'l, d M',
                                         locale: Spanish
                                     }}
-                                    onChange={(val) => (setStateDepartureDate(val))}
+                                    onChange={(val) => (setDepartureDate(val))}
                                 />
                                 <span className='message-error'>Campo obligatorio</span>
                             </div>
@@ -113,7 +120,7 @@ const Home = () => {
                             </div> */}
                             <div className='form-group col-span-2 text-right'>
                                 <button className='btn btn-search'>
-                                    <span className="material-icons">search</span>
+                                    <span className='material-icons'>search</span>
                                     <strong>Buscar</strong>
                                 </button>
                             </div>
