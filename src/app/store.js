d1858b7f1842881e172c4b6d5746501c5eff8863
gmanriqe <gmanriqe@gmail.com>
2022-07-11 14:50:17 -0500

    REDUX - creando mi store y mi slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+// store = lugar donde será almacenado los datos de la aplicación
+// slide = es una parte de un estado
 export const store = configureStore({
     reducer: {
-        // ... your reducer
+        // ... your reducer (un reducer es la tipica funcion que actualiza un estado, tipo el setState de un componente)
     }
 })
---
+++ src/redux/slices/flightSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+export const flightSlice =  createSlice(
+    {
+        name: "flight",
+        initialState: [],
+    }
+)
