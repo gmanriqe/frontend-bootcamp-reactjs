ddb72fdfcc00159e0d9db5ad0ca4d0dbeacf9a3e
gmanriqe <gmanriqe@gmail.com>
2022-07-11 14:55:58 -0500

    REDUX - exportando por defecto el reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import flightReducer from "../redux/slices/flightSlice";
 
 // store = lugar donde será almacenado los datos de la aplicación
 // slide = es una parte de un estado
 export const store = configureStore({
+    flight: flightReducer,
     reducer: {
         // ... your reducer (un reducer es la tipica funcion que actualiza un estado, tipo el setState de un componente)
     }
