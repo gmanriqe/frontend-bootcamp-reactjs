c106b578e8182c0f355ae8f1ec46712550a0c5e7
gmanriqe <gmanriqe@gmail.com>
2022-07-14 13:51:25 -0500

    LOADING - configurando store

---
+++ src/redux/store/index.js
@@ -0,0 +1,19 @@
+/**
+ * Store
+ */
+import { configureStore } from "@reduxjs/toolkit";
+import reducers from "../reducers"; // reducer principal
+
+// configurando mi store
+export default configureStore({
+    // ... your reducer (un reducer es la tipica "funcion" que actualiza un estado, tipo el setState de un componente)
+    reducer: reducers,
+})
+
+/**
+ * Terminos en Redux Toolkit
+ */
+// store = lugar donde será almacenado los datos de la aplicación y seran accedidos desde cualquier parte de la aplicación
+// slide = es una parte de un estado
+// useDispatch = función que vamos a usar para ...
+// useSelector = función que vamos a usar para obtener el estado
