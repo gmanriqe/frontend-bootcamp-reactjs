cb8e4194d40ed7aa02bf71e3d6290c5078d184c9
gmanriqe <gmanriqe@gmail.com>
2022-07-15 02:29:09 -0500

    RUTA - mostrando el detalle de un resultado

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -1,3 +1,32 @@
-const Detail = () => <h6>Detalle de un resultado</h6>
+// 1ero: Paquetes de terceros
+import { useParams } from "react-router-dom"
+
+// 2do: Paquetes de mi propio proyecto
+import { Banner } from '../../components/Banner';
+
+// RTK
+import { useSelector } from 'react-redux';
+
+const Detail = () => {
+    const data = useSelector(state => state.results.data)
+    const { flightId } = useParams()
+    
+    let dataFilter = data.filter(item => item.id === flightId)
+    console.log(dataFilter)
+    
+    return (
+        <div className="main main-detail">
+            <Banner title='DETALLE DEL VUELO' />
+            <section className='main main-results'>
+                <div className='container mx-auto'>
+                    <div className='container-small pt-lg'>
+                        <p>Detalle del vuelo</p>
+                        { dataFilter[0].id} -  
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
 
 export default Detail
