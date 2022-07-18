8928e29217c573292713458899cf463b21d97119
gmanriqe <gmanriqe@gmail.com>
2022-07-17 19:24:00 -0500

    API SELECT PAISES - /desde donde?/ carga valores desde el api y los valores para /¿a donde quiere ir?/ sera llenado por los mismo, pero quitanto el seleccionado previamente

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,3 @@
-export const configHeader = {
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-}
-
 export const clientCredential = {
     grant_type: 'client_credentials',
     client_id: 'ni727jFPcT0nRr7QDX14yZC6eeySq2y8',
---
+++ src/utils/sortArrayForName.js
@@ -0,0 +1,13 @@
+const sortArrayForName = (array, name) => {
+    return array.sort((a, b) => {
+        if (a[name] < b[name]) {
+            return -1;
+        }
+        if (a[name] > b[name]) {
+            return 1;
+        }
+        return 0;
+    });
+}
+
+export default sortArrayForName;
