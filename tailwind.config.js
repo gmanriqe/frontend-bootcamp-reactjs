dfc0d138672d6edd680be1d6bbd74c07b8056539
gmanriqe <gmanriqe@gmail.com>
2022-07-22 15:57:24 -0500

    RESPONSIVE - configurando container

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,46 +5,47 @@ module.exports = {
     purge: ['./src/**/*.{html,js}'],
     darkMode: false,
     theme: {
-        gap: { // defaults to theme => theme('spacing') as per Tailwind 1.2
-            '0': '0',
-            '1': '0.25rem',
-            '2': '0.5rem',
-            '3': '0.75rem',
-            '4': '1rem',
+        extend: {
+            fontFamily: {
+                lato: ['Lato', 'sans-serif'],
+                montserrat: ['Montserrat', 'sans-serif']
+            },
+            fontSize: {
+                'xs': '.75rem',
+                'sm': '.875rem',
+                'tiny': '.875rem',
+                'base': '1rem',
+                'lg': '1.125rem',
+                'xl': '1.25rem',
+                '2xl': '1.5rem',
+                '3xl': '1.875rem',
+                '4xl': '2.25rem',
+                '5xl': '3rem',
+                '6xl': '4rem',
+                '7xl': '5rem',
+            },
+            color: {
+                "white": "#FFF",
+                "black": "#000",
+                ...defaultTheme.colors,
+            },
+            spacing: {
+                xs: '8px',
+                sm: '16px',
+                md: '32px',
+                lg: '64px',
+            },
+            gap: { // defaults to theme => theme('spacing') as per Tailwind 1.2
+                '0': '0',
+                '1': '0.25rem',
+                '2': '0.5rem',
+                '3': '0.75rem',
+                '4': '1rem',
+            },
+            container: {
+                padding: '15px'
+            },
         },
-        fontFamily: {
-            lato: ['Lato', 'sans-serif'],
-            montserrat: ['Montserrat', 'sans-serif']
-        },
-        color: {
-            "white": "#FFF",
-            "black": "#000",
-            ...defaultTheme.colors,
-        },
-        fontSize: {
-            'xs': '.75rem',
-            'sm': '.875rem',
-            'tiny': '.875rem',
-            'base': '1rem',
-            'lg': '1.125rem',
-            'xl': '1.25rem',
-            '2xl': '1.5rem',
-            '3xl': '1.875rem',
-            '4xl': '2.25rem',
-            '5xl': '3rem',
-            '6xl': '4rem',
-            '7xl': '5rem',
-        },
-        spacing: {
-            xs: '8px',
-            sm: '16px',
-            md: '32px',
-            lg: '64px',
-        },
-        container: {
-            padding: '15px'
-        },
-        extend: {},
         variants: {},
         plugins: []
     }
